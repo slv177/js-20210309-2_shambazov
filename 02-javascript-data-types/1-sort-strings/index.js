68a0c4723af153337a37e5afee0f08e7d8959ff2
Vyacheslav Shambazov <shambazov@gmail.com>
2021-03-13 18:56:17 +0100

    Edited description.

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -2,7 +2,7 @@
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
- * @returns {this[]}
+ * @returns {string[]}
  */
 
 export function sortStrings(arr, param = 'asc') {
