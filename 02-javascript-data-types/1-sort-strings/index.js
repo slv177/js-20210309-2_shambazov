72de13bec239dce2cd400c3629a2abeff65e2a95
Vyacheslav Shambazov <shambazov@gmail.com>
2021-03-13 12:42:49 +0100

    Added task sort.

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -2,8 +2,16 @@
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
- * @returns {string[]}
+ * @returns {this[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+export function sortStrings(arr, param = 'asc') {
+  let result = [...arr];
+  result = result.sort(function (a, b) {
+    return a.localeCompare(b, ["ru-ru-u-kf-upper"], { sensitivity: "case", caseFirst: "upper" });
+  });
+  if (param === 'desc') {
+    result.reverse();
+  }
+  return result;
 }
