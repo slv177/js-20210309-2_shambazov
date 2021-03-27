8d2ba9ce64b7ee480f9a7d356001d73ff1eed592
Vyacheslav Shambazov <shambazov@gmail.com>
2021-03-27 11:55:16 +0100

    TODO: убрать повторяющийся код не проходит предпоследний тест

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,79 @@
 export default class NotificationMessage {
+  constructor(message, options = {}) {
+    this.text = message || '123';
+    this.duration = options['duration'] || 1000;
+    this.type = options['type'] || 'unknown type';
+
+    if (this.type === 'success') {
+      this.render_success();
+    } else  {
+      this.render_error();
+    }
+  }
+
+  static isDisplay = false;
+
+  render_success(){
+  const element = document.createElement('div');
+  element.innerHTML = this.template_success;
+  this.element = element.firstElementChild;
+  }
+
+  get template_success() {
+    return `
+    <body>
+       <div class="notification success" style="--value:2s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">success</div>
+          <div class="notification-body">
+            ` + this.text + `
+          </div>
+        </div>
+      </div>
+    </body>
+    `;
+  }
+
+  render_error(){
+    const element = document.createElement('div');
+    element.innerHTML = this.template_error;
+    this.element = element.firstElementChild;
+  }
+
+  get template_error() {
+    return `
+    <body>
+       <div class="notification error" style="--value:2s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">success</div>
+          <div class="notification-body">
+            ` + this.text + `
+          </div>
+        </div>
+      </div>
+    </body>
+    `;
+  }
+
+
+  show (elementToDisplay = this.element) {
+    console.log(elementToDisplay)
+    if (!NotificationMessage.isDisplay) {
+      document.body.append(elementToDisplay);
+      NotificationMessage.isDisplay = true;
+      setTimeout(() => this.remove(), this.duration);
+    }
+  }
+
+  remove () {
+    this.element.remove();
+    NotificationMessage.isDisplay = false;
+  }
+
+  destroy() {
+    this.remove();
+  }
 
 }
