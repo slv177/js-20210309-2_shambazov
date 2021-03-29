548be475672b19700704cdb4406c573baca79c1d
ss <shambazov@gmail.com>
2021-03-29 16:41:46 +0200

    Все работает, но не проходит ни один тест. TypeError: Cannot read property '0' of undefined

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -10,23 +10,102 @@ export default class SortableTable {
     this.render(this.data);
   }
 
-  render(dataToRender){
-    const element = document.createElement('div');
-    element.innerHTML = this.tableHeader;
+  render(dataToRender) {
+    let element = document.createElement('div');
+    element.innerHTML = this.tableTemplate;
     this.element = element.firstElementChild;
+    document.body.append(element);
 
-    const tableRows = (document.createElement('div'));
-    tableRows.innerHTML = this.tableRows(dataToRender);
-    document.body.appendChild(tableRows);
+    let tableHeaderRow = (document.createElement('div'));
+    tableHeaderRow.innerHTML = this.tableHeaderRow;
+    // element.appendChild(tableHeaderRow);
+    let tableHeaderRowCollection = tableHeaderRow.children;
+    let header = document.querySelector(".sortable-table__header")
+    while (tableHeaderRowCollection.length > 0)
+    {
+      header.appendChild(tableHeaderRowCollection[0])
+    }
 
-    const tableFooter = (document.createElement('div'));
-    tableFooter.innerHTML = this.tableFooter;
-    document.body.appendChild(tableFooter);
+    let tableBodyRows = (document.createElement('div'));
+    tableBodyRows.innerHTML = this.tableBodyRow(dataToRender);
+    let tableBodyRowCollection = tableBodyRows.children;
+    let body = document.querySelector(".sortable-table__body")
+    while (tableBodyRowCollection.length > 0)
+    {
+      element.appendChild(tableBodyRowCollection[0])
+    }
+
+    console.log("finally", element);
   }
 
-  sort(fieldValue, orderValue){
+  get tableTemplate(){
+    return `
+          <div data-element="productsContainer" class="products-list__container">
+              <div class="sortable-table">
+                  <div data-element="header" class="sortable-table__header sortable-table__row"></div>
+
+                  <div data-element="body" class="sortable-table__body">
+
+
+                    <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+                  </div>
+
+                  <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
+                    <div>
+                      <p>No products satisfies your filter criteria</p>
+                      <button type="button" class="button-primary-outline">Reset all filters</button>
+                    </div>
+                  </div>
+              </div>
+          </div>
+  `;
+  }
+
+  get tableHeaderRow() {
+    let result = '';
+    // result += this.header.forEach(element =>
+    //   console.log('<div className="sortable-table__cell">' + element['title'] +'</div>')
+    // );
+
+    for (const item of this.header) {
+      result += '<div class="sortable-table__cell" data-id="' + item.id + '" data-sortable="false" data-order="asc"><span>' + item.title + '</span></div>'
+    }
+
+    return result;
+  }
+
+  tableBodyRow(dataToDisplay) {
+    let result = '';
+
+    for (const item of dataToDisplay) {
+      result += `
+                <a href=" ${item['id']} " class="sortable-table__row">
+                    <div class="sortable-table__cell">
+                        <img class="sortable-table-image" alt="Image" src="${item['images'][0]['url']}">
+                    </div>
+                    <div class="sortable-table__cell"> ${item['title']}1</div>
+                    <div class="sortable-table__cell"> ${item['quantity']}</div>
+                    <div class="sortable-table__cell"> ${item['price']}</div>
+                    <div class="sortable-table__cell"> ${item['sales']}</div>
+              </a>
+      `
+    }
+
+    return result;
+  }
+
+  removeElementsByClass(className){
+    let elements = document.getElementsByClassName(className);
+    while (elements.length > 0){
+      elements[0].parentNode.removeChild(elements[0]);
+    }
+  }
+
+  sort(fieldValue, orderValue) {
     this.data = this.sortStrings(fieldValue, orderValue);
+    this.removeElementsByClass('sortable-table__header');
     this.removeElementsByClass('sortable-table__body');
+    this.removeElementsByClass('products-list__container');
     this.render(this.data);
   }
 
@@ -43,78 +122,6 @@ export default class SortableTable {
       }
       return a[field].localeCompare(b[field], ["ru-ru-u-kf-upper"], {sensitivity: "case"});
     });
-  }
-
-  get tableHeader(){
-    const tableHeader = `<div data-element="productsContainer" class="products-list__container">
-      <div class="sortable-table">
-
-      <div data-element="header" class="sortable-table__header sortable-table__row">
-          <div class="sortable-table__cell" data-id="images" data-sortable="false" data-order="asc">
-            <span>Image</span>
-          </div>
-          <div class="sortable-table__cell" data-id="title" data-sortable="true" data-order="asc">
-            <span>Name</span>
-            <span data-element="arrow" class="sortable-table__sort-arrow">
-              <span class="sort-arrow"></span>
-            </span>
-          </div>
-          <div class="sortable-table__cell" data-id="title" data-sortable="true" data-order="asc">
-            <span>Quantity</span>
-          </div>
-          <div class="sortable-table__cell" data-id="quantity" data-sortable="true" data-order="asc">
-            <span>Price</span>
-          </div>
-          <div class="sortable-table__cell" data-id="price" data-sortable="true" data-order="asc">
-            <span>Sales</span>
-          </div>
-        </div>`;
-    return tableHeader;
-  }
-
-  tableHeaderRow(){
-    this.header.forEach(element =>
-      console.log('<div className="sortable-table__cell">' + element['title'] +'</div>')
-
-  )
-  }
-
-  get tableFooter(){
-    const tableFooter = `<div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
-          <div>
-            <p>No products satisfies your filter criteria</p>
-            <button type="button" class="button-primary-outline">Reset all filters</button>
-          </div>
-        </div>
-
-      </div>
-    </div>`;
-    return tableFooter;
-  }
-
-  tableRows(dataToDisplay){
-    this.tableHeaderRow();
-    return '<div data-element="body" class="sortable-table__body">' + dataToDisplay
-      .map (item => {
-        return `
-           <a href=" ${item['id']} " class="sortable-table__row">
-            <div class="sortable-table__cell">
-              <img class="sortable-table-image" alt="Image" src="${item['images'][0]['url']}">
-            </div>
-            <div class="sortable-table__cell"> ${item['title']}1</div>
-            <div class="sortable-table__cell"> ${item['quantity']}</div>
-            <div class="sortable-table__cell"> ${item['price']}</div>
-            <div class="sortable-table__cell"> ${item['sales']}</div>
-          </a>
-        `;})
-      .join('') + '</div><div data-element="loading" class="loading-line sortable-table__loading-line"></div>';
-  }
-
-  removeElementsByClass(className){
-    let elements = document.getElementsByClassName(className);
-    while (elements.length > 0){
-      elements[0].parentNode.removeChild(elements[0]);
-    }
   }
 
   destroy() {
